674b80cb10f62d1e46ef1223f87225671bb421cd
Harshit Seth <78683955+HarshitSeth-809014@users.noreply.github.com>
2023-01-01 17:20:15 +0530

    Implemented broomsticks

--- Information.py
+++ Information.py
@@ -1,5 +1,6 @@
 import json
 
+import discord
 import requests
 import nextcord
 
@@ -7,13 +8,18 @@ SPELL_API = json.loads(requests.get('https://hp-api.onrender.com/api/spells').te
 CHARACTERS_API = json.loads(requests.get('https://hp-api.onrender.com/api/characters').text)
 STUDENTS_API = json.loads(requests.get('https://hp-api.onrender.com/api/characters/students').text)
 PROFESSORS_API = json.loads(requests.get('https://hp-api.onrender.com/api/characters/staff').text)
+with open('./data/broomsticks.json', 'r') as f:
+    BROOMSTICK_API = json.load(f)
 
 
 async def create_embed(api, message, check):
     for val in api:
-        if val['name'].lower() == check:
+        if val['name'].lower() == check.lower():
             embed = nextcord.Embed(title=val['name'])
-            embed.add_field(name="House:", value=val['house'], inline=True)
+            if val['image']:
+                embed.set_image(url=val['image'])
+            if val['house']:
+                embed.add_field(name="House:", value=val['house'], inline=True)
             if val['actor']:
                 embed.add_field(name="Role played by:", value=val['actor'], inline=True)
             if val['alive']:
@@ -28,15 +34,14 @@ async def create_embed(api, message, check):
             embed.add_field(name="Professor at Hogwarts:", value=val['hogwartsStaff'], inline=True)
             if val['gender']:
                 embed.add_field(name="Gender", value=val['gender'], inline=True)
-            if val['image']:
-                embed.set_thumbnail(url=val['image'])
+
 
             await message.response.send_message(embed=embed)
-    await message.response.send_message("Name not found!!!")
+    await message.response.send_message("Name not found!!! Try Again")
 
 
 async def wand_information(message, wand):
-    await message.send(f'all {wand} information')
+    await message.response.send_message(f'all {wand} information')
 
 
 async def professors_character_information(message, professor):
@@ -54,8 +59,14 @@ async def any_character_information(message, character):
 async def spell_information(message, spell):
     for val in SPELL_API:
         if spell == val['name'].lower():
-            await message.send(f'Spell: ```{val["name"]}```\nUse: ```{val["description"]}```')
+            await message.response_send_message(f'Spell: ```{val["name"]}```\nUse: ```{val["description"]}```')
 
 
 async def broomsticks_information(message, broomstick):
-    await message.send(f'all {broomstick} info')
+    for val in BROOMSTICK_API:
+        if val['name'].lower() == broomstick.lower():
+            embed = discord.Embed(title=val['name'], description=val['description'])
+            if val['image']:
+                embed.set_image(url=val['image'])
+            await message.response.send_message(embed=embed)
+    await message.response.send_message('Broomstick not found!!! Try again.')
--- main.py
+++ main.py
@@ -8,14 +8,9 @@ intents.message_content = True
 client = commands.Bot(command_prefix='/', intents=intents)
 
 
-@client.slash_command(description="test")
-async def test(interaction: nextcord.Interaction, name):
-    await interaction.response.send_message(f'hi {name}')
-
-
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user.name}!')
+    print(f'Logged in as {client.user.name}#{client.user.id}!')
 
 
 @client.slash_command(description="Will give you information about any character present in Harry Potter "
@@ -48,7 +43,8 @@ async def wand(message: nextcord.Interaction, name):
     await Information.wand_information(message, name)
 
 
-@client.command()
+@client.slash_command(description="Will give you information about any Broomstick present in "
+                                  "Harry Potter Novel Series", guild_ids=[1057997352926986260])
 async def broomstick(message: nextcord.Interaction, name):
     await Information.broomsticks_information(message, name)
 
