e103d9b40a61870ba987ba9bc140aefdc1a39b83
Harshit Seth <78683955+HarshitSeth-809014@users.noreply.github.com>
2023-01-05 20:39:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,50 +23,48 @@ async def on_ready():
 
 
 @client.slash_command(description="Will give you information about any character present in Harry Potter "
-                                  "Novel Series", guild_ids=[1057997352926986260, 897737630064721930])
+                                  "Novel Series")
 async def character(interaction: nextcord.Interaction, name):
     await Information.any_character_information(interaction, name)
 
 
 @client.slash_command(description="Will give you information about any Professor teaching at Hogwarts in "
-                                  "Harry Potter Novel Series", guild_ids=[1057997352926986260, 897737630064721930])
+                                  "Harry Potter Novel Series")
 async def professor(message: nextcord.Interaction, name):
     await Information.professors_character_information(message, name)
 
 
 @client.slash_command(description="Will give you information about any Student studying at Hogwarts in "
-                                  "Harry Potter Novel Series", guild_ids=[1057997352926986260, 897737630064721930])
+                                  "Harry Potter Novel Series")
 async def student(message: nextcord.Interaction, name):
     await Information.students_character_information(message, name)
 
 
 @client.slash_command(description="Will give you information about any Spell used in "
-                                  "Harry Potter Novel Series", guild_ids=[1057997352926986260, 897737630064721930])
+                                  "Harry Potter Novel Series"])
 async def spell(message: nextcord.Interaction, name):
     await Information.spell_information(message, name)
 
 
 @client.slash_command(description="Will give you information about any Wand present in "
-                                  "Harry Potter Novel Series", guild_ids=[1057997352926986260, 897737630064721930])
+                                  "Harry Potter Novel Series"])
 async def wand(message: nextcord.Interaction, owner):
     await Information.wand_information(message, owner)
 
 
 @client.slash_command(description="Will give you information about any Broomstick present in "
-                                  "Harry Potter Novel Series", guild_ids=[1057997352926986260, 897737630064721930])
+                                  "Harry Potter Novel Series"])
 async def broomstick(message: nextcord.Interaction, name):
     await Information.broomsticks_information(message, name)
 
 
-@client.slash_command(description="Will give you information about any book related to Harry Potter",
-                      guild_ids=[1057997352926986260, 897737630064721930])
+@client.slash_command(description="Will give you information about any book related to Harry Potter")
 async def book(message: nextcord.Interaction, id: int):
     await Information.books_information(message, id)
 
 
 @client.slash_command(description="Name the command like "
-                                  "'book', 'broomstick', 'character', 'professor', 'spell', 'student' and 'wand'",
-                      guild_ids=[1057997352926986260, 897737630064721930])
+                                  "'book', 'broomstick', 'character', 'professor', 'spell', 'student' and 'wand'")
 async def help(message, command: str):
     await Help.all_help(message=message, com=command)
 
